dea38c887252216bc7c6a6325fbf480605c0d770
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-03-28 12:55:54 +0000

    Consultas

--- src/main/java/br/gov/sp/fatec/springtopicos20231/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/springtopicos20231/repository/UsuarioRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.gov.sp.fatec.springtopicos20231.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+    public Usuario findByNome(String nome);
     
 }
