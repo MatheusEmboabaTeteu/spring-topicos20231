2ac08dfbf2728a2b582a8866dec67f411ca862ca
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-04-18 02:53:24 +0000

    Spring Security com JWT

--- src/main/java/br/gov/sp/fatec/springtopicos20231/service/UsuarioService.java
+++ src/main/java/br/gov/sp/fatec/springtopicos20231/service/UsuarioService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import br.gov.sp.fatec.springtopicos20231.entity.Usuario;
@@ -25,6 +26,7 @@ public class UsuarioService implements IUsuarioService {
         return usuarioRepo.save(usuario);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     public List<Usuario> buscarTodosUsuarios() {
         return usuarioRepo.findAll();
     }
